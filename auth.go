d7ce8fdf447340a64e8202ecf41327bffd5fffba
Johannes Scribante <johannes.scribs@gmail.com>
2023-06-08 21:27:48 +0200

    bug/0 - fixed all of the auth headers as well as the request payload

--- auth.go
+++ auth.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // Credentials is the required information to be able to authenticate the user
@@ -33,19 +34,28 @@ func (s *Service) Auth(credentials *Credentials) (string, error) {
 	// set the auth path
 	s.URL.Path = "identity/v2/oauth2/token"
 	basic := encodeBasic(credentials.ClientID, credentials.Secret)
-	req, err := http.NewRequest("POST", s.URL.String(), nil)
+	// create the request form payload
+	payload := strings.NewReader("grant_type=client_credentials")
+
+	req, err := http.NewRequest("POST", s.URL.String(), payload)
 	if err != nil {
 		return "", err
 	}
 	// Headers are case-insensitive
 	req.Header.Add("accept", "application/json")
-	req.Header.Add("authentication", fmt.Sprintf("Basic %s", basic))
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	req.Header.Add("authorization", fmt.Sprintf("Basic %s", basic))
 	req.Header.Add("x-api-key", credentials.APIKey)
 
 	res, err := s.DoRequest(req)
 	if err != nil {
 		return "", err
 	}
+	if res.StatusCode != 200 {
+		log.Printf("Investec Error: %s", res.Status)
+		return "", fmt.Errorf("investec: authentication failed status: %s", res.Status)
+	}
+
 	body := struct {
 		AccessToken string `json:"access_token"`
 		TokenType   string `json:"token_type"`
@@ -54,11 +64,9 @@ func (s *Service) Auth(credentials *Credentials) (string, error) {
 	}{}
 	err = s.MarshalResponseJSON(res, &body)
 	if err != nil {
+		fmt.Println("error marshalling response: to print json body enable env DEBUG=1")
 		return "", err
 	}
-	if res.StatusCode != 200 {
-		log.Printf("Investec Error: %d %s", res.StatusCode, res.Status)
-		return "", nil
-	}
+
 	return body.AccessToken, nil
 }
